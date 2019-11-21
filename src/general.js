c201f95d86a3a3262e86efc82de8ec42b4b8e8b0
RomenaS <aqua3man@gmail.com>
2019-11-21 16:53:42 +0200

    Slices + Bug fix

--- src/general.js
+++ src/general.js
@@ -75,4 +75,107 @@ var videoPlaylistSlider = new Swiper('.videoPlaylistSlider.swiper-container', {
     }
 });
 
+
+/* Sticky sidebar */
+var stickVarTop = getStickOffset();
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+function getStickOffset() {
+  var offsetTOP = null;
+  var sticky = document.getElementsByClassName("sticky-here");
+  if (typeof div !== 'undefined' && typeof div[0] !== 'undefined') {
+    var offsetTOP = offset(sticky[0]).top;
+  }
+
+  return offsetTOP;
+}
+window.onscroll = function() {stickySidebar()};
+function stickySidebar() {
+  var doc = document.documentElement;
+  var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+  var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  var div = document.getElementsByClassName("sticky-here");
+  if (typeof div !== 'undefined' && typeof div[0] !== 'undefined' && window.innerWidth > 991) {
+    var transY = top;
+    div[0].style.transform = "translateY("+transY+"px)";
+  }
+
+}
+/* Sticky sidebar */
+
+/* Animate handler range input */
+var inputRange = document.getElementsByClassName('rangefinder')[0],
+    maxValue = 100, // the higher the smoother when dragging
+    speed = 1,
+    currValue, rafID;
+    // set min/max value
+    inputRange.min = 0;
+    inputRange.max = maxValue;
+    // bind events
+    inputRange.addEventListener('mousedown', unlockStartHandler, false);
+    inputRange.addEventListener('mousestart', unlockStartHandler, false);
+    inputRange.addEventListener('mouseup', unlockEndHandler, false);
+    inputRange.addEventListener('touchend', unlockEndHandler, false);
+
+
+function unlockStartHandler() {
+    // clear raf if trying again
+    window.cancelAnimationFrame(rafID);
+
+    // set to desired value
+    currValue = +this.value;
+}
+function unlockEndHandler() {
+
+    // store current value
+    currValue = +this.value;
+
+    // determine if we have reached success or not
+    if(currValue >= maxValue) {
+        //successHandler();
+    }
+    else {
+        rafID = window.requestAnimationFrame(animateHandler);
+    }
+}
+function animateHandler() {
+
+    // calculate gradient transition
+    var transX = currValue - maxValue;
+
+    // update input range
+    inputRange.value = currValue;
+
+    // determine if we need to continue
+
+    if(currValue > -1 && currValue != 25 && currValue != 50 && currValue != 75 && currValue != 100) {
+      window.requestAnimationFrame(animateHandler);
+    }
+    // decrement value
+    if(currValue > 0 && currValue < 12) {
+      currValue = currValue - speed;
+    }else if (currValue >= 12 && currValue < 25) {
+      currValue = currValue + speed;
+    }else if (currValue >= 25 && currValue < 37) {
+      currValue = currValue - speed;
+    }else if (currValue >= 37 && currValue < 50) {
+      currValue = currValue + speed;
+    }else if (currValue >= 50 && currValue < 62) {
+      currValue = currValue - speed;
+    }else if (currValue >= 62 && currValue < 75) {
+      currValue = currValue + speed;
+    }else if (currValue >= 75 && currValue < 87) {
+      currValue = currValue - speed;
+    }else if (currValue >= 87 && currValue < 100) {
+      currValue = currValue + speed;
+    }else{
+      currValue = currValue - speed;
+    }
+}
+/* Animate handler range input */
+
 /*Scrollbar*/
